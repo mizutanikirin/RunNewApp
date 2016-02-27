550dde931fa78479b290bd4cc91ccfeef212765a
mizutanikirin <mizutani@thedesignium.com>
2016-02-28 01:28:42 +0900

    init commit

---
+++ RunNewApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PortableDevices
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RUN NEW APP");
+            System.Threading.Thread.Sleep(1000);
+            
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            process.StartInfo.FileName = args[0];
+            process.Start();
+
+        }
+    }
+}
